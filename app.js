cbec1fd6391bc177fdc163320bff07e5aa831fb2
Chanaphat Suzuki <yochi.suz@gmail.com>
2021-08-17 15:22:48 +0700

    .

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require("./routes/routes"))
+app.use(require("./routes/routes"))//
 
 app.use(errorHandle)
 module.exports = app;
